bbdc818f6d0679a7ae638a9d837e7be25e0152d7
tneedham <tneedham@TNEEDHAM.meridian.local>
2017-12-12 10:04:29 +0000

    Day 12

--- AdventOfCode2017/Day12.fs
+++ AdventOfCode2017/Day12.fs
@@ -3,12 +3,54 @@ module Day12
 open System;
 open System.IO;
 
+type Connection = {
+    Id : int;
+    Ends : int list
+};;
+
+let parse (s : string) : Connection =
+    match s.Split([| " <-> " |], StringSplitOptions.RemoveEmptyEntries) with
+    | [| a; b |] -> let es = b.Split([| ", " |], StringSplitOptions.RemoveEmptyEntries)
+                            |> Seq.toList
+                            |> List.map (Int32.Parse);
+
+                    { Id = Int32.Parse a; Ends = es };
+
+let addUnique (i : int) (is : int list) : int list =
+    match List.tryFind (fun x -> i = x) is with
+    | Some _ -> is;
+    | None -> (i::is) |> List.sort;
+
+let rec graph (os : int list) (c : Connection) (cs : Connection list) : int list =
+    match c.Ends with
+    | [] -> addUnique c.Id os
+    | xs -> let os' = addUnique c.Id os;
+            let xs' = xs
+                    |> List.where (fun x -> os' |> List.tryFind (fun y -> y = x) = None);
+            let ys = xs'
+                    |> List.map (fun x -> List.find (fun y -> y.Id = x) cs)
+                    |> List.fold (fun a c -> graph a c cs) os';
+            ys;
+
+let rec graphs (cs : Connection list) : int list list =
+    match cs with
+    | [] -> []
+    | x::xs ->  let gs = graph [] x cs;
+                let cs' = cs
+                        |> List.where (fun x -> gs |> List.tryFind (fun y -> y = x.Id) = None);
+                gs::(graphs cs');
+
 let run (file : string) =
 
-    let input = Seq.toList(File.ReadLines(file));
+    let input = Seq.toList(File.ReadLines(file))
+                |> List.map (parse);
 
-    1
+    
+    graph [] (List.head input) input
+    |> List.length
     |> printfn "Day 12, part 1: %d";
 
-    2
+    input
+    |> graphs
+    |> List.length
     |> printfn "Day 12, part 2: %d";
