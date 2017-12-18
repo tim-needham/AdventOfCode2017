d5f584ba9731e8c28f367ac47b87d8ef527e2334
tneedham <tneedham@TNEEDHAM.meridian.local>
2017-12-18 11:59:22 +0000

    Day 17 - why calculate what you don't need?

--- AdventOfCode2017/Day17.fs
+++ AdventOfCode2017/Day17.fs
@@ -3,20 +3,37 @@ module Day17
 open System;
 open System.IO;
 
-let rec spin (p : int) (s : int) (m : int) (i : int) (is : int list) : int list =
-    match i with
-    | x when x = m ->   let js, ks = is |> List.splitAt (p+1);
-                        js@[m]@ks;
-    | _ ->  //if i%1000 = 0 then
-            //    printfn "%d" i;
-            let js, ks = is |> List.splitAt (p+1);
-            let ls = i::(ks@js);
-            spin (s%(i+1)) s m (i+1) ls;
+let insert (p : int) (i : int) (is : int []) : int [] =
+    if p >= is.Length then
+        Array.append is [| i |]
+    else
+        is.[p..(is.Length-1)]
+        |> Array.append [| i |]
+        |> Array.append is.[0..(p-1)];
 
-let key (m : int) (t : int) (s : int) : int =
-    let js = spin 0 s m 1 [0];
-    let i = js |> List.findIndex (fun x -> x = t)
+let rec spin (p : int) (s : int) (m : int) (i : int) (is : int []) : int [] =
+    match i with
+    | x when x = m -> insert (p+1) m is;
+    | _ ->  let ls = insert (p+1) i is;
+            spin ((p+1+s)%(i+1)) s m (i+1) ls;
+
+let keyA (m : int) (t : int) (s : int) : int =
+    let js = spin 0 s m 1 [| 0 |];
+    let i = js |> Array.findIndex (fun x -> x = t)
     js.[(i+1)%m];
+
+let rec quickSpin (p : int) (s : int) (m : int) (i : int) (ss : int list) : int =
+    match i with
+    | x when x = m -> if p = 0 then
+                        m
+                      else
+                        ss |> List.head;
+    | _ ->  let ss' =   if p = 0 then
+                            i::ss;
+                        else
+                            ss;
+
+            quickSpin ((p+1+s)%(i+1)) s m (i+1) ss'
 
 let run (file : string) =
 
@@ -24,9 +41,8 @@ let run (file : string) =
                 |> Int32.Parse;
 
     input
-    |> key 2017 2017
+    |> keyA 2017 2017
     |> printfn "Day 17, part 1: %d";
 
-    input
-    |> key 50000000 0
+    quickSpin 0 input 50000000 1 []
     |> printfn "Day 17, part 2: %d";
