2caddb6a23660213fb0b17ae302c61fe1ee7729e
tim-needham <timothy@MacBook-Pro-2.home>
2017-12-25 12:03:10 +0000

    Day 25 - fast but mutable

--- AdventOfCode2017/Day25.fs
+++ AdventOfCode2017/Day25.fs
@@ -11,76 +11,69 @@ type State =
     | E
     | F;;
 
-let update (c : int) (v : int) (ts : int list) : int list =
-    ts
-    |> List.mapi (fun i x -> if i=c then v else x);
-
-let move (c : int) (ts : int list) (i : int) : int list * int =
-    if i = 1 then
-        if c >= ts.Length - 1 then ts@[0], c+1 else ts, c+1;
-    else
-        if c = 0 then 0::ts, 0 else ts, c-1; 
-
-let test (ts : int list) (c : int) (s : State) : int list * int * State =
-    match s with
-    | A ->  let ts', c' =   if ts.[c] = 0 then
-                                move c (update c 1 ts) 1;
-                            else
-                                move c (update c 0 ts) -1;
-            ts', c', B;
-    | B ->  let ts', c' =   if ts.[c] = 0 then
-                                move c (update c 1 ts) -1;
-                            else
-                                move c (update c 1 ts) 1;
-            ts', c', A;
-
-let exec (ts : int list) (c : int) (s : State) : int list * int * State =
+let update (c : int) (v : int) (ts : int []) : int [] =
+    ts.[c] <- v;
+    ts;
+let test (ts : int []) (c : int) (s : State) : int [] * int * State =
     match s with
     | A ->  if ts.[c] = 0 then
-                let ts', c' = move c (update c 1 ts) 1;
-                ts', c', B;
+                update c 1 ts |> ignore;
+                ts, c+1, B;
             else
-                let ts', c' = move c (update c 0 ts) -1;
-                ts', c', F;
+                update c 0 ts |> ignore;
+                ts, c-1, B;
     | B ->  if ts.[c] = 0 then
-                let ts', c' = move c ts 1;
-                ts', c', C;
+                update c 1 ts |> ignore;
+                ts, c-1, A;
             else
-                let ts', c' = move c (update c 0 ts) 1;
-                ts', c', D;
-    | C ->  if ts.[c] = 0 then
-                let ts', c' = move c (update c 1 ts) -1;
-                ts', c', D;
-            else
-                let ts', c' = move c ts 1;
-                ts', c', E;
-    | D ->  if ts.[c] = 0 then
-                let ts', c' = move c ts -1;
-                ts', c', E;
-            else
-                let ts', c' = move c (update c 0 ts) -1;
-                ts', c', D;
-    | E ->  if ts.[c] = 0 then
-                let ts', c' = move c ts 1;
-                ts', c', A;
-            else
-                let ts', c' = move c ts 1;
-                ts', c', C;
-    | F ->  if ts.[c] = 0 then
-                let ts', c' = move c (update c 1 ts) -1;
-                ts', c', A;
-            else
-                let ts', c' = move c ts 1;
-                ts', c', A;
+                update c 1 ts |> ignore;
+                ts, c+1, A;
 
-let rec simulate (n : int) (ts : int list) (c : int) (s : State) : int list * int * State =
+let exec (ts : int []) (c : int) (s : State) : int [] * int * State =
+    match s with
+    | A ->  if ts.[c] = 0 then
+                update c 1 ts |> ignore;
+                ts, c+1, B;
+            else
+                update c 0 ts |> ignore;
+                ts, c-1, F;
+    | B ->  if ts.[c] = 0 then
+                ts, c+1, C;
+            else
+                update c 0 ts |> ignore;
+                ts, c+1, D;
+    | C ->  if ts.[c] = 0 then
+                update c 1 ts |> ignore;
+                ts, c-1, D;
+            else
+                ts, c+1, E;
+    | D ->  if ts.[c] = 0 then
+                ts, c-1, E;
+            else
+                update c 0 ts |> ignore;
+                ts, c-1, D;
+    | E ->  if ts.[c] = 0 then
+                ts, c+1, A;
+            else
+                ts, c+1, C;
+    | F ->  if ts.[c] = 0 then
+                update c 1 ts |> ignore;
+                ts, c-1, A;
+            else
+                ts, c+1, A;
+
+let rec simulate (n : int) (ts : int []) (c : int) (s : State) : int [] =
     match n with
-    | 0 -> ts, c, s;
+    | 0 -> ts;
     | _ ->  let ts', c', s' = exec ts c s;
             simulate (n-1) ts' c' s';
 
 let run (file : string) =
 
-    simulate 12994925 0 [0] A
-    |> List.sum
+    //let input = Seq.toList(File.ReadLines(file));
+
+    let mutable ts = Array.replicate 12000 0;
+
+    simulate 12994925 ts 6000 A
+    |> Array.sum
     |> printfn "Day 25, part 1: %d";
